7b0632b0073da717c5279309ce8885dc62747f52
Gustavo Eklund <gustavo04616@gmail.com>
2020-10-15 22:43:05 -0300

    added orm annotations

--- src/Domain/Entity/_DefaultEntity.php
+++ src/Domain/Entity/_DefaultEntity.php
@@ -4,20 +4,86 @@ namespace Domain\Entity;
 
 use Application\Validation\DateTimeValidator;
 use DateTime;
+use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
 /**
  * Class _DefaultEntity
  * @package Domain\Entity
+ *
+ * @ORM\MappedSuperclass()
  */
 class _DefaultEntity
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(
+     *     name="uuid",
+     *     type="uuid",
+     *     unique=true,
+     *     options={"comment":"Uuidv4"}
+     * )
+     */
     private UuidInterface $uuid;
+
+    /**
+     * @ORM\Column(
+     *     name="active",
+     *     type="boolean",
+     *     nullable=false,
+     *     options={
+     *     	   "comment": "Define se o registro está ativo ou não",
+     *     	   "default": true
+     *	   }
+     * )
+     */
     private bool $active;
+
+    /**
+     * @ORM\Column(
+     *     name="created_at",
+     *     type="datetime",
+     *     nullable=false,
+     *     options={
+     *     	   "comment": "Data da criação deste registro",
+     * 		   "default": "CURRENT_TIMESTAMP"
+     *     }
+     * )
+     */
     private DateTime $created_at;
+
+    /**
+     * @ORM\Column(
+     *     name="updated_at",
+     *     type="datetime",
+     *     nullable=false,
+     *     options={
+     *         "comment": "Data da última atualização neste registro",
+     *	       "default": "CURRENT_TIMESTAMP"
+     *	   }
+     * )
+     */
     private DateTime $updated_at;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(
+     *     name="created_by",
+     *     referencedColumnName="uuid",
+     *     onDelete="CASCADE"
+     * )
+     */
     private User $created_by;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(
+     *     name="updated_by",
+     *     referencedColumnName="uuid",
+     *     onDelete="CASCADE"
+     * )
+     */
     private User $updated_by;
 
     public function __construct()
